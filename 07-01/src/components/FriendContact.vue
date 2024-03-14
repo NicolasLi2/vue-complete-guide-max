80352dd957998942a29c94f32278a7434e89dc40
nicolas <linze201@gmail.com>
2024-03-14 17:08:27 +0800

    07-01 first vue app

---
+++ 07-01/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>
+      {{ friend.name }}
+    </h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <p>Phone: {{ friend.phone }}</p>
+        <p>Email: {{ friend.email }}</p>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 1,
+        name: 'Max',
+        phone: '1234567890',
+        email: 'max@email.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
