e12b3eca6b86be4a4d43827bdea458557cbe2cdc
nicolas <linze201@gmail.com>
2024-03-14 16:04:21 +0800

    introduction to component

---
+++ 06-01/app.js
@@ -0,0 +1,53 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '1234567890',
+          email: 'manuel@email.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '1234567890',
+          email: 'julie@email.com',
+        },
+      ],
+    };
+  },
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+  <h2>{{friend.name}}</h2>
+  <button @click="toggleDetails">
+    {{detailsAreVisible ? 'Hide' : 'Show'}} Details
+  </button>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong> {{friend.phone}}</li>
+    <li><strong>Email:</strong> {{friend.email}}</li>
+  </ul>
+</li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '1234567890',
+        email: 'manuel@email.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount('#app');
