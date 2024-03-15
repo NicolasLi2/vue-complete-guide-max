31c042def87971a9d503b6b7fca9135f89b016fb
nicolas <linze201@gmail.com>
2024-03-15 21:00:51 +0800

    more on slot

--- 09-01/src/App.vue
+++ 09-01/src/App.vue
@@ -7,6 +7,16 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     />
+    <!-- <CourseGoals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['anotherProp'] }}</p>
+      </template>
+    </CourseGoals> -->
+    <CourseGoals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps['anotherProp'] }}</p>
+    </CourseGoals>
   </div>
 </template>
 
@@ -14,12 +24,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
