e79e69114c9c4f35b4900566500dc3896ce7aca0
nicolas <linze201@gmail.com>
2024-03-16 14:21:27 +0800

    create TheResources component

--- 10-01/src/App.vue
+++ 10-01/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <TheHeader title="RememberMe"></TheHeader>
-    <StoredResource :resources="storedResources"></StoredResource>
+    <TheResources></TheResources>
   </div>
 </template>
 
 <script>
-import StoredResource from './components/learning-resources/StoredResource.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    StoredResource,
     TheHeader,
+    TheResources,
   },
   data() {
     return {
---
+++ 10-01/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <BaseCard>
+      <BaseButton
+        @click="setSelectedTab('StoredResource')"
+        :mode="storedResButtonMode"
+        >Stored Resources</BaseButton
+      >
+      <BaseButton
+        @click="setSelectedTab('AddResource')"
+        :mode="addResButtonMode"
+        >Add Resource</BaseButton
+      >
+    </BaseCard>
+    <component :is="selectedTab"></component>
+  </div>
+</template>
+
+<script>
+import StoredResource from './StoredResource.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResource,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'vue',
+          title: 'Official Guide',
+          description: 'The official guide for Vue.js',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The official guide for Vue.js',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'StoredResource' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'AddResource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
