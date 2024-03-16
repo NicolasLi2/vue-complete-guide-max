c4da5aa5ee4764e06516bfb29c74076e232fc4be
nicolas <linze201@gmail.com>
2024-03-16 13:03:36 +0800

    create BaseCard and TheHeader component

--- 10-01/src/App.vue
+++ 10-01/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <StoredResource :resources="storedResources"></StoredResource>
+  <div>
+    <TheHeader title="RememberMe"></TheHeader>
+    <StoredResource :resources="storedResources"></StoredResource>
+  </div>
 </template>
 
 <script>
 import StoredResource from './components/learning-resources/StoredResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
     StoredResource,
+    TheHeader,
   },
   data() {
     return {
