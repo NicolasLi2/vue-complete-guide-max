afd363ce8c1f44f00cf378b2680db0bc60e3ce7f
nicolas <linze201@gmail.com>
2024-03-16 12:10:20 +0800

    create LearningResource component

---
+++ 10-01/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></LearningResource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'vue',
+          title: 'Official Guide',
+          description: 'The official guide for Vue.js',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The official guide for Vue.js',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
