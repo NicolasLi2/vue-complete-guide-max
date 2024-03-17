3fd8a043f80b40b6806c589e5efc1dc10f9a1032
nicolas <linze201@gmail.com>
2024-03-17 21:59:26 +0800

    route meta data; refactor some code

---
+++ 13-01/src/router.js
@@ -0,0 +1,74 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './pages/NotFound.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter }, // load multiple components for a single route
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        }, // Pass route params as props makes it more reusable
+      ],
+    },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        console.log('users beforeEnter');
+        console.log(to, from);
+        next();
+      },
+    },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+  // linkActiveClass: 'active', // Set css class 'router-link-active' to 'active'
+  scrollBehavior(_, _2, savedPosition) {
+    // console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+// Navigation guard
+router.beforeEach((to, from, next) => {
+  console.log('Global beforeEach');
+  console.log(to, from);
+  if (to.meta.needsAuth) {
+    console.log('Needs auth!');
+    next();
+  } else {
+    next();
+  }
+  // next(false); // Prevent navigation
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
+});
+
+router.afterEach((to, from) => {
+  // Sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
+export default router;
