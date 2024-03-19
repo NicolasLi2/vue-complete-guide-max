428c08017bcf215e4e3747a430595b34aa4621ff
nicolas <linze201@gmail.com>
2024-03-19 21:41:44 +0800

    refactor code

---
+++ 15-01/src/store/counter/actions.js
@@ -0,0 +1,11 @@
+export default {
+  // async code
+  increment(context) {
+    setTimeout(() => {
+      context.commit('increment');
+    }, 2000);
+  },
+  increase(context, payload) {
+    context.commit('increase', payload);
+  },
+};
