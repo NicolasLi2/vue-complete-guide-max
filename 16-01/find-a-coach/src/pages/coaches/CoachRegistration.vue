1ff0251fe37fdcded1bfd16c9a42abca5b076a32
nicolas <linze201@gmail.com>
2024-03-23 21:56:14 +0800

    add CoachForm

--- 16-01/find-a-coach/src/pages/coaches/CoachRegistration.vue
+++ 16-01/find-a-coach/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,19 @@
-<template>Register</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseCard from '../../../../../09-01/src/components/BaseCard.vue';
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+    BaseCard,
+  },
+};
+</script>
