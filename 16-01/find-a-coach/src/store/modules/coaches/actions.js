62d91760c1decfd4117eb0a4977c2be1f1c40cbf
nicolas <linze201@gmail.com>
2024-03-26 22:35:25 +0800

    storing new coach data in the firebase realtime database

--- 16-01/find-a-coach/src/store/modules/coaches/actions.js
+++ 16-01/find-a-coach/src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       areas: data.areas,
@@ -9,6 +9,23 @@ export default {
       hourlyRate: data.rate,
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(
+      `https://vue-http-demo-52f5c-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
