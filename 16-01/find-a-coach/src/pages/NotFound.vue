6c9ebc4426d7946bf8432812df578138121c3aeb
Nicolas Leigh <linze201@gmail.com>
2024-03-28 22:58:51 +0800

    create NotFound page

--- 16-01/find-a-coach/src/pages/NotFound.vue
+++ 16-01/find-a-coach/src/pages/NotFound.vue
@@ -1,6 +1,11 @@
 <template>
   <section>
-    <h2>Page not found!</h2>
-    <p>Sorry, the page you are looking for does not exist.</p>
+    <base-card>
+      <h2>Page not found!</h2>
+      <p>
+        Sorry, the page you are looking for does not exist. Maybe check out
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
   </section>
 </template>
