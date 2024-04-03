aa92f5c683c4b959b9380958e30df58c4f130532
Nicolas Leigh <linze201@gmail.com>
2024-04-03 22:00:55 +0800

    components, props & the composition API

--- 17-01/compositionAPI/src/App.vue
+++ 17-01/compositionAPI/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
-    <h3>{{ age }}</h3>
+    <user-data
+      :first-name="firstName"
+      :last-name="lastName"
+      :age="age"
+    ></user-data>
+    <!-- <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3> -->
     <button @click="setAge">Change Age</button>
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
@@ -14,8 +19,12 @@
 <script>
 // import { reactive } from 'vue';
 import { ref, computed, watch } from 'vue';
+import UserData from './components/UserData.vue';
 
 export default {
+  components: {
+    UserData,
+  },
   setup() {
     // const uName = ref('Maximilian');
     const firstName = ref('');
